954484717d7f6f0a4dc8f05c5ee448dc797a4677
Erith <66786887+erith-c@users.noreply.github.com>
2020-09-24 10:21:35 -0400

    0.1.1 - Create SequenceConverter

---
+++ Modules/SequenceConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace AngelLight.Modules
+{
+	public class SequenceConverter
+	{
+		public string Sequence { get; set; }
+		public int Value { get; set; }
+
+		public SequenceConverter()
+		{
+			Sequence = "";
+			Value = 0;
+		}
+		public SequenceConverter( string sequence )
+		{
+			Sequence = sequence;
+			Value = 0;
+		}
+
+		public void UpdateSequence( string sequence ) => this.Sequence = sequence;
+		public void UpdateValue( int value ) => this.Value = value;
+		public void Update( string sequence, int value )
+		{
+			UpdateSequence(sequence); UpdateValue(value);
+		}
+
+		public int GetNumberOfDigits( int value, int numtimes = 1 )
+		{
+			if (value >= this.Sequence.Length)
+			{
+				numtimes++;
+				return GetNumberOfDigits( value / this.Sequence.Length, numtimes );
+			}
+			return numtimes;
+		}
+
+		public string ConvertValue()
+		{
+			string ConvertedValue = "";
+			int NumberOfDigits = this.GetNumberOfDigits( this.Value );
+			int tempmax, tempval = 0;
+
+			for (int i = NumberOfDigits; i > 0; i--)
+			{
+				this.Value -= tempval;
+
+				tempmax = (int)Math.Pow(this.Sequence.Length, i - 1);
+				tempval = this.Value / tempmax;
+				ConvertedValue += this.Sequence[tempval];
+
+				tempval *= tempmax;
+			}
+
+			return ConvertedValue;
+
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AngelLight.Modules;
+using System;
 using System.Buffers.Text;
 using System.ComponentModel;
 using System.Linq;
@@ -42,14 +43,21 @@ namespace ALS.WARDEN
 			Console.WriteLine("//===================================//" +
 							  "    </ THE [OVERSEER] GREETS YOU />    " +
 							  "\\\\===================================\\\\");
-			Console.Write("    [OVERSEER]\n</ PLEASE ENTER A SEQUENCE. />\n  << ");
-			string Sequence = Console.ReadLine();
-			Console.WriteLine($"[Sequence]: \"{Sequence}\"");
 			Console.Write("    [OVERSEER]\n</ PLEASE ENTER A VALUE TO CONVERT. />\n  << ");
 			int Value = Convert.ToInt32(Console.ReadLine());
 
+			SequenceConverter Base10 = new SequenceConverter("0123456789");
+			SequenceConverter Base16 = new SequenceConverter("0123456789ABCDEF");
+			SequenceConverter Base3 = new SequenceConverter("NOS");
+			Base10.UpdateValue(Value);
+			Base16.UpdateValue(Value);
+			Base3.UpdateValue(Value);
+
 			Console.WriteLine("//PROCESSING//");
-			Console.WriteLine($"    [OVERSEER]\n</ THE CONVERTED VALUE IS [{GetCode(Value, Sequence)}] />");
+			Console.WriteLine($"    [OVERSEER]\n</ THE INPUTTED VALUE IS [{Value}] />");
+			Console.WriteLine($"    [OVERSEER]\n</ THE BASE10 VALUE IS [{Base10.ConvertValue()}] />");
+			Console.WriteLine($"    [OVERSEER]\n</ THE CONVERTED BASE16 VALUE IS [{Base16.ConvertValue()}] />");
+			Console.WriteLine($"    [OVERSEER]\n</ THE CONVERTED BASE3 VALUE IS [{Base3.ConvertValue()}] />");
 			Console.WriteLine("    [OVERSEER]\n</ PLEASE RECORD THIS VALUE FOR FUTURE USE. />");
 			Console.WriteLine("\n...//>>");
 		}
