a5a92ffa9dcc4431a37c0c65cf12730d4b3728ea
Erith <66786887+erith-c@users.noreply.github.com>
2020-09-23 23:07:56 -0400

    0.1.0 - Sequencing is working. Yay!

--- Program.cs
+++ Program.cs
@@ -8,138 +8,50 @@ namespace ALS.WARDEN
 {
 	class Program
 	{
+		static int NumberOfDigits(int value, string sequence, int numtimes = 1)
+		{
+			if (value >= sequence.Length)
+			{
+				numtimes++;
+				return NumberOfDigits(value / sequence.Length, sequence, numtimes);
+			}
+			return numtimes;
+		}
+
+		static string GetCode(int value, string sequence)
+		{
+			string code = "";
+			int numdigits = NumberOfDigits(value, sequence);
+			int temp, val = 0;
+
+			for (int i = numdigits; i > 0; i--)
+			{
+				value -= val;
+				temp = (int)Math.Pow(sequence.Length, i - 1);
+				val = value / temp;
+				code += sequence[val];
+				val *= temp;
+			}
+			return code;
+		}
+
 		static void Main(string[] args)
 		{
 			//Core.Start();
-			string base10 = "0123456789";
-			int myvalue = 101;
-			Console.WriteLine("Sequence start:\n<<//...");
+			Console.WriteLine("<<//...\n");
+			Console.WriteLine("//===================================//" +
+							  "    </ THE [OVERSEER] GREETS YOU />    " +
+							  "\\\\===================================\\\\");
+			Console.Write("    [OVERSEER]\n</ PLEASE ENTER A SEQUENCE. />\n  << ");
+			string Sequence = Console.ReadLine();
+			Console.WriteLine($"[Sequence]: \"{Sequence}\"");
+			Console.Write("    [OVERSEER]\n</ PLEASE ENTER A VALUE TO CONVERT. />\n  << ");
+			int Value = Convert.ToInt32(Console.ReadLine());
 
-			//(int x, int y) numtimes = GetRecursion(myvalue, base10);
-			//Console.WriteLine($"<! GetRecursion returned ({numtimes.x}, {numtimes.y}) >");
-			int numdigits = GetNumDigits(myvalue, base10);
-			Console.WriteLine($"<! GetNumDigits returned {numdigits} >");
-			//string v = GetValue(myvalue, base10);
-
+			Console.WriteLine("//PROCESSING//");
+			Console.WriteLine($"    [OVERSEER]\n</ THE CONVERTED VALUE IS [{GetCode(Value, Sequence)}] />");
+			Console.WriteLine("    [OVERSEER]\n</ PLEASE RECORD THIS VALUE FOR FUTURE USE. />");
 			Console.WriteLine("\n...//>>");
 		}
-		
-		static (int, int) GetRecursion(int value, string sequence, int numtimes = 0)
-		{
-			Console.WriteLine($"<<//...In GetRecursion()...//>>");
-			if (value >= sequence.Length)
-			{
-				Console.WriteLine($"<! value: {value, 3}, new test: {value - sequence.Length, 3} >");
-				Console.WriteLine($"<! numtimes: {numtimes, 3}, value: {value - sequence.Length, 3} >");
-				Console.WriteLine();
-				return GetRecursion(value - sequence.Length, sequence, ++numtimes);
-			}
-			Console.WriteLine($"<<//...Returning [{numtimes, 3},{value, 3}]...//>>");
-			return (numtimes, value);
-		}
-
-		static int GetNumDigits(int value, string sequence)
-		{
-			int digits = 1;
-			Console.WriteLine($"<<//...In GetNumDigits()...//>>");
-			(int t, int r) numdigits = GetRecursion(value, sequence);
-			Console.WriteLine($"<! ({numdigits.t},{numdigits.r}) >");
-			if (numdigits.t >= sequence.Length)
-				return digits + GetNumDigits(numdigits.t, sequence);
-			return numdigits.t;
-		}
-
-		static string GetValue(int value, string sequence)
-		{
-			string converted = "";
-			int digits = 1;
-			if (value > sequence.Length)
-			{
-				digits = GetNumDigits(value, sequence);
-			}
-			int tempval = value;
-			for (int i = digits; i > 0; i--)
-			{
-				Console.WriteLine($"<! tempval: {tempval} >");
-				int digmax = (int)Math.Pow(sequence.Length, i - 1);
-				Console.WriteLine($"<! digmax: {digmax} >");
-				int times = 0;
-				do
-				{
-					times++;
-					tempval -= digmax;
-					Console.WriteLine($"<! times: {times}, tempval: {tempval} >");
-				} while (tempval >= digmax);
-				converted += times;
-				Console.WriteLine($"<! converted: {converted} >");
-			}
-			Console.WriteLine($"<<//...Returning [{converted}]...//>>");
-			return converted;
-		}
-
-		//static int GetNumDigits(int value, string sequence)
-		//{
-		//	//Console.WriteLine($"Value: {value}");
-		//	//Console.WriteLine("GetNumDigits()...");
-		//	int numdigits = 1;
-		//	int numtimes = 0;
-		//	if (value > sequence.Length)
-		//	{
-		//		//Console.WriteLine("Getting number of times...");
-		//		numtimes = numtimes + GetNumTimes(value, sequence);
-		//		numdigits++;
-		//	}
-		//	//Console.WriteLine($"Number of times: {numtimes}");
-		//	//Console.WriteLine($"Number of digits: {numdigits}");
-		//	if (numtimes > sequence.Length)
-		//	{
-		//		//Console.WriteLine("<<//OVERSEER IS SEEN//>>");
-		//		//Console.WriteLine($"Recursing...");
-		//		int tempval = numtimes - (numtimes - sequence.Length);
-		//		return numdigits + GetNumDigits(value - sequence.Length*tempval, sequence);
-		//	}
-		//	//Console.WriteLine($"Returning {numdigits}");
-		//	return numdigits;
-		//}
-
-		//static string GetValue(int value, string sequence)
-		//{
-		//	string strval = "";
-		//	int numdigits = GetNumDigits(value, sequence);
-		//	int tempval = value;
-			
-		//	for (int i = numdigits; i > 0; i--)
-		//	{
-		//		Console.WriteLine($"<! i = {i}");
-		//		int maxval = (int)Math.Pow(sequence.Length, i);
-		//		if (tempval > maxval)
-		//		{
-		//			Console.WriteLine($"<! tempval: {tempval}, maxval: {maxval} >");
-		//			Console.WriteLine($"<! {maxval} / {tempval} = {maxval / tempval} >");
-		//			int val = maxval / tempval;
-		//			Console.WriteLine($"<! val is {val} >");
-		//			strval += sequence[val];
-		//			Console.WriteLine($"<! Value: {strval} >");
-		//			tempval -= maxval;
-		//		}
-		//		else if (tempval < maxval)
-		//		{
-		//			Console.WriteLine($"<<//OVERSEER IS SEEN//>>");
-		//			Console.WriteLine($"<! tempval: {tempval}, maxval: {maxval} >");
-		//			int tempmax = (int)Math.Pow(sequence.Length, i - 1);
-		//			Console.WriteLine($"<! tempval: {tempval}, maxval: {tempmax} >");
-		//			Console.WriteLine($"<! {tempval} / {tempmax} = {(int)(tempval / tempmax)} >");
-		//			int val = tempval / tempmax;
-		//			strval += sequence[val];
-		//			Console.WriteLine($"<! Value: {strval}");
-		//		}
-		//		else
-		//		{
-		//			Console.WriteLine($"<<//...null...//>>");
-		//		}
-		//	}
-
-		//	return strval;
-		//}
 	}
 }
